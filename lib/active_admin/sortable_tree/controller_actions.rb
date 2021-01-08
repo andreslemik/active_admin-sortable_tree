b43c78da3f1d2ba41fa9d4902f88e04f78f3b8a9
Andrew Svidersky <andreslemik@yandex.ru>
2021-01-08 18:14:04 +0400

    quick fix for awesome_nested_set

--- lib/active_admin/sortable_tree/controller_actions.rb
+++ lib/active_admin/sortable_tree/controller_actions.rb
@@ -1,55 +1,68 @@
-module ActiveAdmin::SortableTree
-  module ControllerActions
+# frozen_string_literal: true
 
-    attr_accessor :sortable_options
+module ActiveAdmin
+  module SortableTree
+    module ControllerActions
+      attr_accessor :sortable_options
 
-    def sortable(options = {})
-      options.reverse_merge! :sorting_attribute => :position,
-                             :parent_method => :parent,
-                             :children_method => :children,
-                             :roots_method => :roots,
-                             :tree => false,
-                             :max_levels => 0,
-                             :protect_root => false,
-                             :collapsible => false, #hides +/- buttons
-                             :start_collapsed => false,
-                             :sortable => true
+      def sortable(options = {})
+        options.reverse_merge! sorting_attribute: :position,
+                               parent_method: :parent,
+                               parent_attribute: :parent_id,
+                               children_method: :children,
+                               roots_method: :roots,
+                               tree: false,
+                               max_levels: 0,
+                               protect_root: false,
+                               collapsible: false, # hides +/- buttons
+                               start_collapsed: false,
+                               sortable: true
 
-      # BAD BAD BAD FIXME: don't pollute original class
-      @sortable_options = options
+        # BAD BAD BAD FIXME: don't pollute original class
+        @sortable_options = options
 
-      # disable pagination
-      config.paginate = false
+        # disable pagination
+        config.paginate = false
 
-      collection_action :sort, :method => :post do
-        resource_name = ActiveAdmin::SortableTree::Compatibility.normalized_resource_name(active_admin_config.resource_name)
+        collection_action :sort, method: :post do
+          resource_name = ActiveAdmin::SortableTree::Compatibility.normalized_resource_name(active_admin_config.resource_name)
 
-        records = []
-        params[resource_name].each_pair do |resource, parent_resource|
-          parent_resource = resource_class.find(parent_resource) rescue nil
-          records << [resource_class.find(resource), parent_resource]
-        end
-
-        errors = []
-        ActiveRecord::Base.transaction do
-          records.each_with_index do |(record, parent_record), position|
-            record.send "#{options[:sorting_attribute]}=", position
-            if options[:tree]
-              record.send "#{options[:parent_method]}=", parent_record
+          records = []
+          params[resource_name].each_pair do |resource, parent_resource|
+            parent_resource = begin
+              resource_class.find(parent_resource)
+            rescue StandardError
+              nil
             end
-            errors << {record.id => record.errors} if !record.save
+            records << [resource_class.find(resource), parent_resource]
+          end
+
+          errors = []
+          ActiveRecord::Base.transaction do
+            records.each_with_index do |(record, parent_record), idx|
+              prev_elm = idx.zero? ? nil : records[idx - 1]
+              next_elm = idx == records.size - 1 ? nil : records[idx + 1]
+
+              if record.root?
+                record.move_to_root
+              elsif prev_elm
+                record.move_to_right_of(prev_elm)
+              elsif next_elm
+                record.move_to_left_of(next_elm)
+              end
+              record.move_to_child_of(parent_record) if options[:tree] && parent_record
+              errors << { record.id => record.errors } unless record.save
+            end
+          end
+          if errors.empty?
+            head 200
+          else
+            render json: errors, status: 422
           end
         end
-        if errors.empty?
-          head 200
-        else
-          render json: errors, status: 422
-        end
       end
-
     end
 
+    ::ActiveAdmin::ResourceDSL.include ControllerActions
   end
-
-  ::ActiveAdmin::ResourceDSL.send(:include, ControllerActions)
 end
